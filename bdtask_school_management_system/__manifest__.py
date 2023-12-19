f42ad6d496d54e225751db26db135ba97ec27398
Zarin-16 <tasnimzarin427@gmail.com>
2023-12-19 13:12:14 +0600

    [UPDATE]image

--- bdtask_school_management_system/__manifest__.py
+++ bdtask_school_management_system/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'bdtask School Management System',
-    'version': '15.0.0.3',
+    'version': '15.0.0.4',
     'summary': 'School Management System',
     'description': ' School Management System',
     'category': ' School management system',
